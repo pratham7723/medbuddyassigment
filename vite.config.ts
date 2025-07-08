482ecf87ea9895c06a5f569e0dd0c99b0a004790
pratham7723 <prathammehta773@gmail.com>
2025-07-09 01:47:55 +0530

    viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
+// Vite Configuration
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',  // This ensures the output directory is 'dist'
+  },
 })
